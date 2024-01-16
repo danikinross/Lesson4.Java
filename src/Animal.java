1bb7c953c8145406bc7f42c35b13b9168a72e1f9
Dani Kinross <danyakinross@Danis-MBP.lan>
2024-01-16 11:28:28 +0200

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,15 @@
+import com.sun.jdi.InterfaceType;
+
+public abstract class Animal implements IAnimal{
+
+    public String Type;
+    public String Color;
+    public String Kind;
+
+    public Animal(String kind,String color, String type) {
+        this.Color = color;
+        this.Kind = kind;
+        this.Type = type;
+    }
+    public abstract void makeSound();
+}
---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Animal {
+
+    //Constructor
+
+    public Dog (String type, String color) {
+        super("Dog",color,type);
+    }
+
+    //behaviour
+
+    public void makeSound() {
+        System.out.println("Woof Woof");
+    }
+}
